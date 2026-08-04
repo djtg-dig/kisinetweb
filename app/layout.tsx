9b5b1ca5c44614dae28b1de8009147620e00b081
mubaraka <carrivemubaraka28@gmail.com>
2026-08-04 14:46:18 +0100

    résoudre l'erreur d'hydratation React sur l'élément html
    Ajouter suppressHydrationWarning sur l'élément html pour éviter l'incohérence entre le rendu serveur et l'exécution du script de thème qui ajoute la classe dark avant l'hydratation React.

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr">
+    <html lang="fr" suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
